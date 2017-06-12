a419b51842910a5d87389f688b9b45685f76e584
Sean <slangiewicz.21@gmail.com>
2017-06-11 23:47:26 -0700

    Added decreasing energy bar with right mouse click

--- Dragon RPG/Assets/_CameraUI/CameraRaycaster.cs
+++ Dragon RPG/Assets/_CameraUI/CameraRaycaster.cs
@@ -8,7 +8,7 @@ namespace RPG.CameraUI
     public class CameraRaycaster : MonoBehaviour
     {
         // INSPECTOR PROPERTIES RENDERED BY CUSTOM EDITOR SCRIPT
-        [SerializeField] int[] layerPriorities;
+        [SerializeField] int[] layerPriorities = null;
 
 
         float maxRaycastDepth = 100f; // Hard coded value
@@ -20,6 +20,9 @@ namespace RPG.CameraUI
 
         public delegate void OnClickPriorityLayer(RaycastHit raycastHit, int layerHit); // declare new delegate type
         public event OnClickPriorityLayer notifyMouseClickObservers; // instantiate an observer set
+
+        public delegate void OnRightClick(RaycastHit raycastHit, int layerHit); // declare new delegate type
+        public event OnClickPriorityLayer notifyRightClickObservers; // instantiate an observer set
 
 
         void Update()
@@ -51,6 +54,11 @@ namespace RPG.CameraUI
             if (Input.GetMouseButton(0))
             {
                 notifyMouseClickObservers(priorityHit.Value, layerHit);
+            }
+
+            if (Input.GetMouseButton(1))
+            {
+                notifyRightClickObservers(priorityHit.Value, layerHit);
             }
 
         }
